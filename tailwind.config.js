316602dd2094c7a7b97763501acab9de3bc8da4e
emamjion <emamjion@gmail.com>
2024-11-30 19:36:56 +0600

    I changed Website title

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,22 +3,22 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    // container: {
-    //   center: true,
-    //   padding: '20px'
+    container: {
+      center: true,
+      padding: '20px'
+    },
+    // screens: {
+    //   sm: "340px",
+    //   md: "540px",
+    //   lg: "768px",
+    //   xl: "1180px",
     // },
     screens: {
-      sm: "340px",
-      md: "540px",
-      lg: "768px",
-      xl: "1180px",
-    },
-  //   screens: {
-  //     sm: '640px',
-  //     md: '768px',
-  //     lg: '992px',
-  //     xl: '1440px',
-  // },
+      sm: '640px',
+      md: '768px',
+      lg: '992px',
+      xl: '1440px',
+  },
   },
   plugins: [],
 };
